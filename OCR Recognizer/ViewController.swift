4c6d8e3ef55bcd8deca256b3c5b8b749ef92dc2e
Lilya Parilyak <lip@meshtech.no>
2020-05-25 21:15:23 +0300

    Initial Commit

---
+++ OCR Recognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR Recognizer
+//
+//  Created by Lilya on 25.05.2020.
+//  Copyright © 2020 Lilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
